f8193475b09f141b0b297b18ddb5a3ab7a09ecb7
hun7ok <erwansmartdata@gmail.com>
2022-06-25 23:08:59 +0800

    add function gomodmutliples

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hun7ok/gomodmultiple
+
+go 1.18
---
+++ gomodmulti.go
@@ -0,0 +1,9 @@
+package gomodmultiple
+
+func SayGoodMorning() string {
+	return "Good Morning"
+}
+
+func SayGoodNight() string {
+	return "Selamata Mlaam"
+}
